21c9d9152d07731eac917a97380bf8853b69c58b
joel-n <51881100+joel-n@users.noreply.github.com>
2019-06-26 18:52:54 +0200

    new class Chest

---
+++ Chest.java
@@ -0,0 +1,52 @@
+package game;
+
+import java.util.HashMap;
+
+public class Chest extends Item {
+
+	private HashMap<String, Item> content;
+	private int itemWeight;
+	
+	public Chest(String name,int weight, int price, boolean removable) {
+		super(name, weight, price, removable);
+		HashMap<String, Item> content = new HashMap<String, Item>();
+		this.setContent(content);
+	}
+	
+	public void setContent(HashMap<String, Item> content) {
+		this.content = content;
+	}
+	
+	public HashMap<String, Item> getContent(){
+		return this.content;
+	}
+	
+	@Override
+	public int getWeight() {
+		return (this.getWeight() + this.getItemWeight()); 
+	}
+	
+	// ADD AND REMOVE ITEM; WEIGHT IS UPDATED 
+	public void addItem(Item item) {
+		this.getContent().put(item.getName(), item);
+		this.setItemWeight(this.getItemWeight() + item.getWeight());
+	}
+	
+	public void removeItem(String itemName) {
+		this.getContent().remove(itemName);
+		this.setItemWeight(this.getItemWeight() - this.getItem(itemName).getWeight());
+	}
+	
+	public Item getItem(String itemName) {
+		return this.getContent().get(itemName);
+	}
+	
+	public void setItemWeight(int weight) {
+		this.itemWeight = weight;
+	}
+	
+	public int getItemWeight() {
+		return this.itemWeight;
+	}
+	
+}
